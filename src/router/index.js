8836d3fde4599fd036199bf3a45e8209fbe2b239
luifer Almendrales <almendralesluifer@gmail.com>
2022-01-27 17:12:37 -0500

    update

---
+++ src/router/index.js
@@ -0,0 +1,80 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import axios from "axios";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/clients",
+    name: "Clients",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Clients.vue"),
+  },
+  {
+    path: "/detail/:id",
+    name: "Detail",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../components/DetailClient.vue"),
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Login.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+async function validateSesion() {
+  let valid = false;
+  const urlApi = process.env.VUE_APP_URL_API;
+  const token = localStorage.getItem("token");
+  try {
+    const res = await axios.get(urlApi + "validate-sesion", {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    if (res.data) {
+      valid = true;
+    }
+  } catch (error) {
+    if (error.response.status === 401) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("vuex");
+      valid = false;
+    }
+  }
+  return valid;
+}
+
+function existToken() {
+  if (localStorage.getItem("token")) {
+    return true;
+  }
+}
+
+router.beforeEach((to, from, next) => {
+  var isLogin = existToken();
+  if (validateSesion() && isLogin) {
+    next();
+  } else {
+    if (
+      to.name === "Login" /* ||
+      to.name === "Forgot" ||
+      to.name === "ResetPassword" */
+    ) {
+      next();
+    } else {
+      next("login");
+    }
+  }
+});
+
+export default router;
---
+++ src/store/index.js
@@ -0,0 +1,39 @@
+import { createStore } from "vuex";
+import VuexPersistence from "vuex-persist";
+import axios from "axios";
+
+export default createStore({
+  state() {
+    return {
+      user: null,
+    };
+  },
+  mutations: {
+    userLogin(state, user) {
+      state.user = user;
+    },
+    RESET_USER(state) {
+      state.user = null;
+    },
+  },
+  actions: {
+    async logout({ commit }) {
+      let response;
+      const urlApi = process.env.VUE_APP_URL_API;
+      const token = localStorage.getItem("token");
+      try {
+        response = await axios.get(urlApi + "logout", {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        if (response.data.res && localStorage.getItem("token")) {
+          commit("RESET_USER");
+        }
+      } catch (ex) {
+        // Handle error
+        console.log(ex);
+      }
+      localStorage.removeItem("token");
+    },
+  },
+  plugins: [new VuexPersistence().plugin],
+});
